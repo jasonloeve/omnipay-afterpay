c5acba996ee0a16e6c8cf0e2fddfafa3aa63161e
Jason L <jasonloeve@gmail.com>
2019-04-30 10:09:13 +1200

    Init code on repo

---
+++ src/Message/PurchaseResponse.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Omnipay\AfterPay\Message;
+
+use Symfony\Component\HttpFoundation\Response as HttpResponse;
+
+class PurchaseResponse extends Response
+{
+    protected $script = 'https://portal.sandbox.afterpay.com/afterpay.js';
+
+    public function getRedirectMethod()
+    {
+        return 'POST';
+    }
+
+    /**
+     * @return bool
+     */
+    public function isRedirect()
+    {
+        return true;
+    }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function getRedirectResponse()
+    {
+        $output = <<<EOF
+<html>
+<head>
+    <title>Redirecting...</title>
+    <script src="%s" async></script>
+</head>
+<body>
+    <script>
+    window.onload = function() {
+        AfterPay.init(countryCode: "%s");
+        AfterPay.redirect({token: "%s"});
+    };
+    </script>
+</body>
+</html>
+EOF;
+
+        $output = sprintf($output, $this->getScriptUrl(), $this->getCountryCode(), $this->getToken());
+
+        return HttpResponse::create($output);
+    }
+
+    /**
+     * @return string
+     */
+    public function getScriptUrl()
+    {
+        return $this->script;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getToken()
+    {
+        return isset($this->data->token) ? $this->data->token : null;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTransactionReference()
+    {
+        return $this->getToken();
+    }
+}
